d10c9dca641f8ce9ee20d7c8018a8d5a7ad58026
marchuanv <Marchuanv@gmail.com>
2020-08-27 12:36:01 +0200

    Update component.request.js

--- component.request.js
+++ component.request.js
@@ -3,7 +3,12 @@ const logging = require("logging");
 logging.config.add("Sending Request");
 module.exports = { 
     send: ({ host, port, path, method, headers, data, retryCount = 1  }) => {
-        return new Promise((resolve) => {
+        return new Promise((resolve, reject) => {
+            
+            if (typeof data !== "string"){
+                return reject("data provided is not a string");
+            }
+            
             const requestUrl = `${host}:${port}${path}`;
             delete headers["content-length"];
             headers["Content-Length"] = Buffer.byteLength(data);
