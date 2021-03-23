f57259e8f02e21527a6af3096874d431000ac8cc
marchuanv <marchuanv@gmail.com>
2021-03-23 06:34:42 +0200

    refactor

--- component.request.js
+++ component.request.js
@@ -16,7 +16,7 @@ component.register({moduleName: "component.request "}).then((request) => {
             delete headers["content-length"];
             headers["Content-Length"] = Buffer.byteLength(data);
             logging.write("Sending Request",`sending request to ${requestUrl}`);
-            const request = http.request({ host, port, path, method, timeout: 30000, headers }, (response) => {
+            const req = http.request({ host, port, path, method, timeout: 30000, headers }, (response) => {
                 response.setEncoding('utf8');
                 let rawData="";
                 response.on('data', (chunk) => {
@@ -27,18 +27,18 @@ component.register({moduleName: "component.request "}).then((request) => {
                     resolve({ data: rawData, headers: response.headers, statusCode: response.statusCode, statusMessage: response.statusMessage });
                 });
             });
-            request.on('error', async (error) => {
+            req.on('error', async (error) => {
                 logging.write("Sending Request",`error sending request retry ${retryCount} of 3`, error);
                 retryCount = retryCount + 1;
                 if (retryCount <= 3){
                     const res = await module.exports.send({ host, port, path, method, headers, data, retryCount });
                     resolve(res);
                 } else {
-                    resolve({ data: error, headers: request.headers, statusCode: 500, statusMessage: "Connection Error" });
+                    resolve({ data: error, headers: req.headers, statusCode: 500, statusMessage: "Connection Error" });
                 }
             });
-            request.write(data);
-            request.end();
+            req.write(data);
+            req.end();
         });
     }
 });
